5767af8b747f4bb2eb293e3b10f34c4de95a796e
OliverFrey <58007575+OliverFrey@users.noreply.github.com>
2026-04-30 11:00:28 +0200

    Added error handling to the RouteListViewModel.kt

--- composeApp/src/commonMain/kotlin/com/example/ascentlister/route/presentation/route_list/RouteListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/example/ascentlister/route/presentation/route_list/RouteListViewModel.kt
@@ -3,15 +3,21 @@ package com.example.ascentlister.route.presentation.route_list
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.ascentlister.ascent.domain.AscentRepository
+import com.example.ascentlister.core.domain.onError
 import com.example.ascentlister.core.domain.onSuccess
+import com.example.ascentlister.core.presentation.toUiText
 import com.example.ascentlister.route.domain.Route
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -20,9 +26,20 @@ class RouteListViewModel(
 ): ViewModel() {
 
     private var cachedRoutes = emptyList<Route>()
+    private var searchJob: Job? = null
 
     private val _state = MutableStateFlow(RouteListState())
-    val state = _state.asStateFlow()
+    val state = _state
+        .onStart {
+            if(cachedRoutes.isEmpty()){
+                observeSearchQuery()
+            }
+        }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            _state.value
+        )
 
     fun onAction(action: RouteListAction){
             when (action) {
@@ -62,25 +79,30 @@ class RouteListViewModel(
                         )}
                     }
                     query.length >= 3 -> {
-                        searchRoutes(query)
+                        searchJob?.cancel()
+                        searchJob = searchRoutes(query)
                 }
             }
         }.launchIn(viewModelScope)
     }
 
-    private fun searchRoutes(query: String){
-        _state.update { it.copy(
-            isLoading = true
-        ) }
+    private fun searchRoutes(query: String) = viewModelScope.launch {
+            _state.update { it.copy(
+                isLoading = true
+            ) }
 
-        viewModelScope.launch {
             ascentRepository
                 .getAscents(query)
                 .onSuccess { searchResults ->
                     _state.update { it.copy(
                         errorMessage = null,
                         searchResults = searchResults,
-                        errorMessage =
+                    ) }
+                }
+                .onError { error ->
+                    _state.update { it.copy(
+                        searchResults = emptyList(),
+                        errorMessage = error.toUiText()
                     ) }
                 }
         }
